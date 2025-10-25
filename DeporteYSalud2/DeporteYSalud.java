6b6877018d26f35a56217086f05dc4c5b8541833
Skyth85 <154547420+Skyth85@users.noreply.github.com>
2025-10-26 00:58:12 +0200

    Deporte y Salud 2

---
+++ DeporteYSalud2/DeporteYSalud.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deporteysalud;
+
+import java.awt.Font;
+import java.io.InputStream;
+import javax.swing.SwingUtilities;
+/**
+ *
+ * @author david
+ */
+public class DeporteYSalud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
